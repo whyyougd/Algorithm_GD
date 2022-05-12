9c1c99fdb1db9e58033ff90b5f28c397750285e1
whyyougd <holygd73@naver.com>
2022-05-12 21:38:11 +0900

    first commit

---
+++ src/main/java/com/example/algorithm_gd/AlgorithmGdApplication.java
@@ -0,0 +1,13 @@
+package com.example.algorithm_gd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmGdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmGdApplication.class, args);
+    }
+
+}
