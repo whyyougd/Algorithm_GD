456978eec776b7936fdbcc367fee7ca0b79053ef
whyyougd <holygd73@naver.com>
2022-05-12 21:38:22 +0900

    A+B

---
+++ src/main/java/com/example/algorithm_gd/Baekjoon/Q_1000.java
@@ -0,0 +1,15 @@
+package com.example.algorithm_gd.Baekjoon;
+
+import java.util.Scanner;
+
+public class Q_1000 {
+        public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a+b);
+    }
+}
