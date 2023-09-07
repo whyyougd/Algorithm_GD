fef49e1a173b666fec5aa995c904298ff6218bc5
kd.lee <kd.lee@springworks.co.kr>
2023-09-07 15:14:24 +0900

    1차 비밀지도

--- src/main/java/com/example/algorithm_gd/Programmers/kakaoBlind_2.java
+++ src/main/java/com/example/algorithm_gd/Programmers/kakaoBlind_2.java
@@ -1,6 +1,7 @@
 package com.example.algorithm_gd.Programmers;
 
 import java.util.Arrays;
+import java.util.Objects;
 
 //1차 비밀지도
 public class kakaoBlind_2 {
@@ -8,8 +9,8 @@ public class kakaoBlind_2 {
         int n = 5;
         int[] arr1 = {9, 20, 28, 18, 11};
         int[] arr2 = {9, 1, 21, 17, 28};
-        System.out.println("answer: " + Arrays.toString(solution(n, arr1, arr2)));
-        //solution2(n, arr1, arr2);
+        //System.out.println("answer: " + Arrays.toString(solution(n, arr1, arr2)));
+        System.out.println(Arrays.toString(solution2(n, arr1, arr2)));
     }
 
     public static String[] solution(int n, int[] arr1, int[] arr2) {
@@ -32,6 +33,35 @@ public class kakaoBlind_2 {
             answer[i] = str;
         }
         System.out.println(Arrays.toString(sum));
+        return answer;
+    }
+
+    /*
+    다른 방법
+     */
+    public static String[] solution2(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
+
+        for(int i=0; i<n; i++){
+            String str1= Integer.toBinaryString(arr1[i]);
+            String str2= Integer.toBinaryString(arr2[i]);
+
+            while(str1.length()<n){
+                str1 = "0" + str1;
+            }
+            while(str2.length()<n){
+                str2 = "0" + str2;
+            }
+            String row = "";
+            for(int j=0;j<n;j++){
+                if(Objects.equals(str1.split("")[j], "1") || Objects.equals(str2.split("")[j], "1"))
+                    row += "#";
+                else if(Objects.equals(str1.split("")[j], "0") && Objects.equals(str2.split("")[j], "0"))
+                    row += " ";
+            }
+            answer[i] = row;
+        }
+
         return answer;
     }
 }
